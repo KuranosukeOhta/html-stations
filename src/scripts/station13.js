5b8b936f859af4158a471498eb3441116c80f4f0
KuranosukeOhta <ohta.kura@gmail.com>
2024-03-12 08:27:47 +0900

    Station13まで

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -2,10 +2,16 @@ data = "global data";
 
 // このreturnDataに代入される関数の形をアロー関数に変えて"global data"が表示されるようにしてください
 // ↓関数の中の処理は変更しないでください
+/*
 const returnThisData = function () {
   return this.data;
 };
+*/
 // ↑関数の中の処理は変更しないでください
+
+//アロー関数をここに記述
+const returnThisData　= () => this.data;
+
 
 const object = {
   data: "object data",
